23f166cc16585edc89859c0adf7f9906b364d741
8gaU8 <haga.yuya2.2016@gmail.com>
2024-09-26 23:48:20 +0300

    get frequent itemsets: done, get support: not yet

---
+++ CA1/task2.py
@@ -0,0 +1,153 @@
+from utils import TEST_TRACTS
+from task1 import calc_support
+
+
+def get_sorted_items(tracts: list[frozenset]) -> list:
+    """get items from transactions and sort by support counting
+
+    Args:
+        tracts (list[frozenset]): transactions
+
+    Returns:
+        list: items which sorted by support counting
+    """
+    # get unique items
+    items: list[str] = []
+    for tract in tracts:
+        items.extend(list(tract))
+    unique_items = set(items)
+
+    # count support
+    counts: list[tuple[str, int]] = []
+    for item in unique_items:
+        sup = calc_support(set(item), tracts)
+        counts.append((item, sup))
+
+    # sort with support and get only items
+    sorted_items = sorted(counts, key=lambda i_s: -i_s[1])
+
+    def get_0(i_s):
+        return i_s[0]
+
+    items = list(map(get_0, sorted_items))
+    return items
+
+
+def convert_items2itemsets(items: list) -> list[set]:
+    itemsets: list[set] = []
+    for item in items:
+        itemsets.append(set([item]))
+    return itemsets
+
+
+def gen_candidates(frequent_itemsets: list[set], items: list) -> list[set]:
+    """generate candidates from frequent itemsets and unique frequent items
+
+    Args:
+        frequent_itemsets (list[set])
+        items (list): frequent singleton items
+
+    Returns:
+        list[set]: next candidates of frequent itemsets
+    """
+    candidates = []
+    for fi in frequent_itemsets:
+        for item in items:
+            if item in fi:
+                continue
+            candidate = fi.copy()
+            candidate.add(item)
+            if candidate not in candidates:
+                candidates.append(candidate)
+    return candidates
+
+
+def prune_candidates(
+    candidates: list[set],
+    prev_pruned_itemsets: list[set],
+) -> tuple[list[set], list[set]]:
+    """Prune itemsets that violate downward closure
+
+    Args:
+        candidates (list[set]): return of gen_candidates
+        prev_pruned_itemsets (list[set]):
+            candidate itemsets which pruned or not frequent
+
+    Returns:
+        tuple[list[set], list[set]]: survived itemsets, pruned itemsets
+    """
+    if len(prev_pruned_itemsets) == 0:
+        return candidates, []
+    not_pruned = []
+    pruned = []
+    for cand in candidates:
+        violate_flag = False
+        for prev_pruned in prev_pruned_itemsets:
+            if prev_pruned <= cand:
+                violate_flag = True
+                break
+        if violate_flag:
+            pruned.append(cand)
+        else:
+            not_pruned.append(cand)
+
+    return not_pruned, pruned
+
+
+def get_frequent_itemsets(
+    not_pruned: list[set], tracts: list[frozenset], min_supp: int
+) -> tuple[list[set], list[set]]:
+    """count support of each itemset that survived from pruning
+
+    Args:
+        not_pruned (list[set]): return of prune_candidates[0]
+        tracts (list[frozenset]): all transactions of dataset
+        min_supp (int)
+
+    Returns:
+        list[set]: frequent k-th itemsets, and not frequent ones
+    """
+    frequent_itemsets = []
+    non_frequent_itemsets = []
+    for itemset in not_pruned:
+        supp = calc_support(itemset, tracts)
+        if supp >= min_supp:
+            frequent_itemsets.append(itemset)
+        else:
+            non_frequent_itemsets.append(itemset)
+    return frequent_itemsets, non_frequent_itemsets
+
+
+def main():
+    items = get_sorted_items(TEST_TRACTS)
+    assert items == ["d", "a", "b", "c"]
+    print(f"{items = }")
+
+    frequent_itemsets = []
+
+    items = get_sorted_items(TEST_TRACTS)
+    itemsets = convert_items2itemsets(items)
+
+    candidates = gen_candidates(itemsets, items)
+    candidates, pruned = prune_candidates(candidates, list())
+    fi, non_fi = get_frequent_itemsets(candidates, TEST_TRACTS, 2)
+    pruned.extend(non_fi)
+    frequent_itemsets.extend(fi)
+
+    candidates = gen_candidates(fi, items)
+    candidates, pruned = prune_candidates(candidates, pruned)
+    fi, non_fi = get_frequent_itemsets(candidates, TEST_TRACTS, 2)
+    pruned.extend(non_fi)
+    frequent_itemsets.extend(fi)
+
+    candidates = gen_candidates(fi, items)
+    candidates, pruned = prune_candidates(candidates, pruned)
+    fi, non_fi = get_frequent_itemsets(candidates, TEST_TRACTS, 2)
+    pruned.extend(non_fi)
+    frequent_itemsets.extend(fi)
+    # ! サポート数を出力する
+    print(f"{frequent_itemsets = }")
+
+
+if __name__ == "__main__":
+    main()
