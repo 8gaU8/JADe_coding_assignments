8db07120aec9af105a69212be247d5202ae963c5
8gaU8 <haga.yuya2.2016@gmail.com>
2024-09-26 22:49:52 +0300

    task1

---
+++ task1.py
@@ -0,0 +1,21 @@
+from utils import TEST_TRACTS
+
+
+def calc_support(itemset: "set", tracts: "list[frozenset]") -> int:
+    support = 0
+    for tract in tracts:
+        if itemset <= set(tract):
+            support += 1
+    return support
+
+
+def main():
+    itemset = "abd"
+    itemset = set(list(itemset))
+    support = calc_support(itemset, TEST_TRACTS)
+    assert support == 2
+    print(f"{support = }")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,30 @@
+from fim_resources import DATASETS, load_data_txt, load_matrix, load_trans_txt
+
+
+def get_transactions(dataset: str) -> tuple[list, list]:
+    # transported from fim_resources:main
+    FUNC_MAP = {
+        "data_txt": load_data_txt,
+        "matrix": load_matrix,
+        "tans_txt": load_trans_txt,
+    }
+    if DATASETS.get(dataset, None) is None:
+        raise ValueError(f"no such dataset {dataset}")
+
+    file_path = DATASETS[dataset]["in_file"]
+    func_label = DATASETS[dataset]["format"]
+    load_method = FUNC_MAP[func_label]
+    params = DATASETS[dataset].get("params", {})  # type: ignore
+    tracts, U = load_method(file_path, **params)  # type: ignore
+    return tracts, U
+
+
+# small transactions from ex1,2,3
+TEST_TRACTS = [
+    frozenset(["c", "d"]),
+    frozenset(["a"]),
+    frozenset(["a", "b", "d"]),
+    frozenset(["b", "d"]),
+    frozenset(["a", "b", "c", "d"]),
+    frozenset(["a", "d"]),
+]
