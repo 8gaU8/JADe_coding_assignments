136925582b29a90f8326915c9da28519a75cc3b3
8gaU8 <haga.yuya2.2016@gmail.com>
2024-09-28 23:14:48 +0300

    numpy版も実装。かなり遅い。

---
+++ CA1/task_np.py
@@ -0,0 +1,158 @@
+import numpy as np
+from utils import TIME_STAMPS, log_ts
+
+
+@log_ts
+def get_unique_items(tracts: list[frozenset]) -> set:
+    unique_items = set()
+    for tract in tracts:
+        unique_items |= tract
+    return unique_items
+
+
+@log_ts
+def mk_onehot(tract: frozenset | list, item_map: dict) -> np.ndarray:
+    a = np.array([item_map[item] for item in tract])
+    one_hot = np.zeros(len(item_map.keys()), dtype=np.int8)
+    one_hot[a] = 1
+    return one_hot
+
+
+@log_ts
+def get_tracts_mat(tracts: list[frozenset], item_map: dict) -> np.ndarray:
+    tracts_mat = np.array(
+        [mk_onehot(tract, item_map) for tract in tracts], dtype=np.int8
+    )
+    return tracts_mat
+
+
+@log_ts
+def calc_support(itemset: np.ndarray, tracts_mat: np.ndarray):
+    idx = np.where(itemset == 1)[0]
+    item_row = tracts_mat[..., idx]
+    item_num = item_row.sum(axis=1)
+    return (item_num == idx.shape).sum()
+
+
+@log_ts
+def get_frequent_itemsets(
+    itemsets: np.ndarray,
+    tracts_mat: np.ndarray,
+    min_supp: int,
+):
+    frequent_itemsets = []
+    freq_supp = []
+    non_frequent_itemsets = []
+    non_freq_supp = []
+    for itemset in itemsets:
+        supp = calc_support(itemset, tracts_mat)
+        if supp >= min_supp:
+            frequent_itemsets.append(itemset)
+            freq_supp.append(supp)
+        else:
+            non_frequent_itemsets.append(itemset)
+            non_freq_supp.append(supp)
+    return (
+        (frequent_itemsets, freq_supp),
+        (non_frequent_itemsets, non_freq_supp),
+    )
+
+
+@log_ts
+def gen_candidates(
+    frequent_itemsets: list[np.ndarray], items_ary: np.ndarray
+) -> np.ndarray:
+    freqs = np.array(frequent_itemsets)
+    k = freqs.sum(axis=1).max()
+    reps = items_ary.shape[0]
+    candiates = np.tile(freqs, reps=(reps, 1))
+    reps = freqs.shape[0]
+    buddy = items_ary.repeat(reps, axis=0)
+
+    candiates += buddy
+    candiates = (candiates > 0).astype(np.int8)
+    candiates = np.unique(candiates, axis=0)
+    candiates = candiates[candiates.sum(axis=1) > k]
+    return candiates
+
+
+@log_ts
+def prune_candidates(
+    candidates: np.ndarray,
+    pruned: list[np.ndarray],
+) -> tuple[np.ndarray, list[np.ndarray]]:
+
+    if len(pruned) == 0:
+        return candidates, []
+
+    k = pruned[0].sum()
+    prune_indexes = []
+    all_index = set(range(candidates.shape[0]))
+    for p in pruned:
+        prune_index = np.where(((candidates + p) >= k).sum(axis=1) >= k)[0]
+        prune_indexes.extend(prune_index)
+    prune_indexes = set(prune_indexes)
+    cand_indexes = all_index.difference(prune_indexes)
+    next_candidates = candidates[list(cand_indexes)]
+    next_pruned = candidates[list(prune_indexes)]
+    return next_candidates, list(next_pruned)
+
+
+@log_ts
+def main():
+    from utils import get_transactions
+
+    tracts, U = get_transactions("plants")
+
+    items = get_unique_items(tracts)
+    item_map = {e: i for i, e in enumerate(items)}
+    tracts_mat = get_tracts_mat(tracts, item_map)
+    min_supp = 3000
+
+    frequent_itemsets = []
+    frequent_supp = []
+    pruned = []
+
+    items_ary = np.array([mk_onehot([item], item_map) for item in items])
+
+    # itemsets = np.array([mk_onehot([item]) for item in items])
+    freq, non_freq = get_frequent_itemsets(items_ary, tracts_mat, min_supp)
+
+    frequent_itemsets.extend(freq[0])
+    frequent_supp.extend(freq[1])
+    pruned.extend(non_freq[0])
+    print(len(frequent_itemsets))
+
+    pruned = []
+    while True:
+        candidates = gen_candidates(freq[0], items_ary)
+        candidates, pruned = prune_candidates(candidates, pruned)
+
+        freq, non_freq = get_frequent_itemsets(
+            candidates,
+            tracts_mat,
+            min_supp,
+        )
+        if len(freq[0]) == 0:
+            break
+
+        frequent_itemsets.extend(freq[0])
+        frequent_supp.extend(freq[1])
+        pruned.extend(non_freq[0])
+        print(len(frequent_itemsets))
+    import pickle
+
+    filename = "np-data.pickle"
+    with open(filename, "wb") as f:
+        pickle.dump(frequent_itemsets, f)
+    filename = "np-supp.pickle"
+    with open(filename, "wb") as f:
+        pickle.dump(frequent_supp, f)
+
+
+if __name__ == "__main__":
+    from pprint import pprint as print
+
+    print(TIME_STAMPS)
+    main()
+    print(TIME_STAMPS)
