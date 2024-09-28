b4a2a16f04b15b0c48a7bb3958e921970d98b71e
8gaU8 <haga.yuya2.2016@gmail.com>
2024-09-28 23:14:31 +0300

    fix typo

--- CA1/task3.py
+++ CA1/task3.py
@@ -7,7 +7,7 @@ from utils import TEST_TRACTS, get_transactions, TIME_STAMPS
 
 def main():
     tracts, U = get_transactions("plants")
-    fi = apriori(tracts, min_supp=3000  )
+    fi = apriori(tracts, min_supp=3000)
     filename = datetime.now().strftime("normal.pickle")
 
     with open(filename, "wb") as f:
